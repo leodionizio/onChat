93f68c9da28e6d1bf956c216f768f1ab4d0ff6aa
Leonardo Dionizio <leodionizio13@gmail.com>
2017-10-02 08:17:23 -0300

    regex

---
+++ src/shared/regex.ts
@@ -0,0 +1,7 @@
+const regexEmail = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
+
+const regex = {
+    regexEmail
+}
+
+export default regex;
