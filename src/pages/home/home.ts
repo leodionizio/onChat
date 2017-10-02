fa6657944ac9fb27486ec3252513382acc2676d3
Leonardo Dionizio <leodionizio13@gmail.com>
2017-10-02 08:15:00 -0300

    criado tela inicial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,36 @@
+import { FirebaseListObservable } from 'angularfire2';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+import { UserService } from './../../providers/user/user.service';
+
+import { SignupPage } from './../signup/signup';
+import { User } from './../../models/user.model';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public users: FirebaseListObservable<User[]>;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public userService: UserService
+  ) {
 
+  }
+
+  ionViewDidLoad() {
+    this.users = this.userService.users;
+  }
+
+  public onSignup(): void {
+    this.navCtrl.push(SignupPage);
+  }
+
+  public onChatCreate(user: User): void {
+    console.log(user);
   }
 
 }
