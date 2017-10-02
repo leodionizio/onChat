de492efef667127315dc1a36225269e00277c1e0
Leonardo Dionizio <leodionizio13@gmail.com>
2017-10-02 08:15:44 -0300

    importação dos modulos e das configurações do firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { HttpModule } from '@angular/http';
+
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { SignupPage } from './../pages/signup/signup';
+
+import { AuthService } from './../providers/auth/auth.service';
+import { UserService } from './../providers/user/user.service';
+
+import { AngularFireModule, FirebaseAppConfig } from 'angularfire2';
+
+
+const firebaseAppConfig: FirebaseAppConfig = {
+  apiKey: "AIzaSyCZDwEUGXldx2kcjIYBmIlGfUP1bnRfd2I",
+  authDomain: "talkfly-5311c.firebaseapp.com",
+  databaseURL: "https://talkfly-5311c.firebaseio.com",
+  storageBucket: "talkfly-5311c.appspot.com",
+  messagingSenderId: "1093142937640"
+}
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    SignupPage
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseAppConfig),
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    SignupPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    UserService,
+    AuthService,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
   ]
 })
 export class AppModule {}
