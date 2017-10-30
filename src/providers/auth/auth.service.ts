140d92780100b0f6ca9e2804cd4bcf5a62cbe694
Leonardo Dionizio <leodionizio13@gmail.com>
2017-10-30 08:17:53 -0200

    criado serviço de autenticação

--- src/providers/auth/auth.service.ts
+++ src/providers/auth/auth.service.ts
@@ -1,18 +1,44 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/first';
 
 import { AngularFireAuth, FirebaseAuthState } from 'angularfire2';
+import { BaseService } from './../base/base.service';
 
 @Injectable()
-export class AuthService {
+export class AuthService extends BaseService {
 
   constructor(
     public http: Http,
     public auth: AngularFireAuth
-  ) { }
+  ) {
+    super();
+  }
 
   public createAuthUser(user: { email: string, password: string }): firebase.Promise<FirebaseAuthState> {
-    return this.auth.createUser(user);
+    return this.auth.createUser(user)
+      .catch(this.handlePromiseError);
+  }
+
+  public signinWithEmail(user: { email: string, password: string }): firebase.Promise<boolean> {
+    return this.auth.login(user)
+      .then((authState: FirebaseAuthState) => {
+        return authState != null;
+      }).catch(this.handlePromiseError);
+  }
+
+  public logout(): Promise<void> {
+    return this.auth.logout();
+  }
+
+  get authenticated(): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      this.auth
+        .first()
+        .subscribe((authState: FirebaseAuthState) => {
+          authState ? resolve(true) : reject(false);
+        })
+    })
   }
 }
