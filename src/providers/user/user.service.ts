06fa57a127dc9f5d5642e3a2eed648a234299603
Leonardo Dionizio <leodionizio13@gmail.com>
2017-10-30 08:17:13 -0200

    adicionado serviço de user

--- src/providers/user/user.service.ts
+++ src/providers/user/user.service.ts
@@ -1,25 +1,81 @@
-import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Injectable, Inject } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 import { User } from './../../models/user.model';
-import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { AngularFire, FirebaseListObservable, FirebaseObjectObservable, FirebaseAuthState, FirebaseApp } from 'angularfire2';
+import { BaseService } from './../base/base.service';
 
 @Injectable()
-export class UserService {
+export class UserService extends BaseService {
 
   users: FirebaseListObservable<User[]>
-
+  currentUser: FirebaseObjectObservable<User>;
   constructor(
     public af: AngularFire,
+    @Inject(FirebaseApp) public firebaseApp: any,
     public http: Http
   ) {
-    this.users = this.af.database.list(`/users`);
+    super();
+    this.listenAuthState();
+
+
   }
 
-  public create(user: User): firebase.Promise<void> {
-    return this.users
-      .push(user);
+  private setUsers(uidToExclude: string): void {
+    this.users = <FirebaseListObservable<User[]>>this.af.database.list(`/users`, {
+      query: {
+        orderByChild: 'name'
+      }
+    }).map((users: User[]) => {
+      return users.filter((user: User) => user.$key !== uidToExclude);
+    })
+  }
+
+  private listenAuthState(): void {
+    this.af.auth.subscribe((authState: FirebaseAuthState) => {
+      if (authState) {
+        this.currentUser = this.af.database.object(`/users/${authState.auth.uid}`);
+        this.setUsers(authState.auth.uid);
+      }
+    })
+  }
+
+  public create(user: User, uuid: string): firebase.Promise<void> {
+    return this.af.database.object(`/users/${uuid}`)
+      .set(user)
+      .catch(this.handlePromiseError);
+  }
+
+  public edit(user: { name: string, username: string, photo: string }): firebase.Promise<void> {
+    return this.currentUser
+      .update(user)
+      .catch(this.handlePromiseError);
+  }
+
+  public userExists(username: string): Observable<boolean> {
+    return this.af.database.list(`/users`, {
+      query: {
+        orderByChild: 'username',
+        equalTo: username
+      }
+    }).map((users: User[]) => {
+      return users.length > 0;
+    });
+  }
+
+  public get(userId: string): FirebaseObjectObservable<User> {
+    return <FirebaseObjectObservable<User>>this.af.database.object(`/users/${userId}`)
+      .catch(this.handleObservableError);
+  }
+
+  public uploadPhoto(file: File, userId: string): firebase.storage.UploadTask {
+    return this.firebaseApp
+      .storage()
+      .ref()
+      .child(`/users/${userId}`)
+      .put(file);
   }
 
 }
