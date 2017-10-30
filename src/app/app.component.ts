72600c0249a7e8de4e0a4a2d217a14d72b5f8593
Leonardo Dionizio <leodionizio13@gmail.com>
2017-10-30 08:21:47 -0200

    configurações no app module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,52 @@
+import { HomePage } from './../pages/home/home';
+import { FirebaseAuthState } from 'angularfire2';
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
+import { User } from './../models/user.model';
+import { AuthService } from './../providers/auth/auth.service';
+import { UserService } from './../providers/user/user.service';
+
+import { SigninPage } from './../pages/signin/signin';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any;
+  currentUser: User;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    authService: AuthService,
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    userService: UserService
+  ) {
+
+    authService.auth.subscribe((authState: FirebaseAuthState) => {
+      if (authState) {
+        this.rootPage = HomePage;
+        userService.currentUser
+          .subscribe((user: User) => {
+            this.currentUser = user;
+          })
+      } else {
+        this.rootPage = SigninPage;
+      }
+    })
+
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+    // console.log(this.authService.authenticated)
+
   }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,23 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
+import { CapitalizePipe } from './../pipes/capitalize.pipe';
+import { ChatPage } from './../pages/chat/chat';
+import { CustomLoggedHeaderComponent } from './../components/custom-logged-header/custom-logged-header.component';
 import { HomePage } from '../pages/home/home';
+import { MessageBoxComponent } from './../components/message-box/message-box.component';
+import { SigninPage } from './../pages/signin/signin';
 import { SignupPage } from './../pages/signup/signup';
+import { UserInfoComponent } from './../components/user-info/user-info.component';
+import { UserMenuComponent } from './../components/user-menu/user-menu.component';
+import { UserProfilePage } from './../pages/user-profile/user-profile';
 
 import { AuthService } from './../providers/auth/auth.service';
+import { ChatService } from '../providers/chat/chat.service';
+import { MessageService } from './../providers/message/message.service';
 import { UserService } from './../providers/user/user.service';
 
-import { AngularFireModule } from 'angularfire2';
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
 
 // arquivo de configurações do firebase, que vc obtém através do seu console no próprio firebase
 // importado de outro arquivo restrito.
@@ -28,29 +38,49 @@ import firebaseAppConfig from './firebase.config';
 //  messagingSenderId: " /// "
 // }
 
+const firebaseAuthConfig = {
+  provider: AuthProviders.Custom,
+  method: AuthMethods.Password
+}
+
 @NgModule({
   declarations: [
     MyApp,
+    CapitalizePipe,
+    ChatPage,
+    CustomLoggedHeaderComponent,
+    MessageBoxComponent,
     HomePage,
-    SignupPage
+    SigninPage,
+    SignupPage,
+    UserInfoComponent,
+    UserMenuComponent,
+    UserProfilePage
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    AngularFireModule.initializeApp(firebaseAppConfig),
-    IonicModule.forRoot(MyApp)
+    AngularFireModule.initializeApp(firebaseAppConfig, firebaseAuthConfig),
+    IonicModule.forRoot(MyApp, {
+      mode: 'md'
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
+    ChatPage,
     HomePage,
-    SignupPage
+    SigninPage,
+    SignupPage,
+    UserProfilePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    UserService,
     AuthService,
+    ChatService,
+    MessageService,
+    UserService,
     { provide: ErrorHandler, useClass: IonicErrorHandler },
   ]
 })
